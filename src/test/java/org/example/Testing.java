eb096f6191816d688b369904698d3e97f096a243
Diskobots <chrisccain@gmail.com>
2024-06-26 21:24:59 -0400

    3rd Push

--- src/test/java/org/example/Testing.java
+++ src/test/java/org/example/Testing.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Testing {
     public static void main(String[] args) {
-        System.out.println("My third git class");
+        System.out.println("Hello World");
+        System.out.println("Getting this thing done");
+        System.out.println("MONEY MONEY MONEY");
     }
 }
